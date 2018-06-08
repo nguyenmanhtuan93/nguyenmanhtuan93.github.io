4282b868aa6400f4f0b606eb8c7cdff6832f6d16
Nguyen Manh Tuan <tuan.nguyenm5@nashtechglobal.com>
2018-06-08 17:38:58 +0700

    edit gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,8 +5,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
-var livereload = require('gulp-livereload');
-var minifyCss = require('gulp-cssnano');
+// var livereload = require('gulp-livereload');
+// var minifyCss = require('gulp-cssnano');
 let cleanCSS = require('gulp-clean-css');
 let sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
@@ -14,18 +14,30 @@ var reload      = browserSync.reload;
 
 // Compile Our Sass
 gulp.task('sass', function () {
-    return gulp.src('styles/*.scss')
-        .pipe(sass())
-        .pipe(minifyCss())
+    return gulp.src('./styles/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(concat('styles.css'))
+        .pipe(cleanCSS({debug: true}, (details) => {
+            console.log(`${details.name}: ${details.stats.originalSize}`);
+            console.log(`${details.name}: ${details.stats.minifiedSize}`);
+        }))
+        .pipe(sourcemaps.write())
         .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('styles'))
-        .pipe(livereload());
+        .pipe(gulp.dest('./styles'))
+        .pipe(browserSync.stream());
 });
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-    livereload.listen();
-    gulp.watch(['styles/*.scss'], ['sass']);
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("./styles/scss/*.scss", ['sass']);
+    gulp.watch("./javascripts/*.js").on('change', reload);
+    gulp.watch("./*.html").on('change', reload);
 });
 
 // Default Task
