bc04ce4f1b160af9e31234354e799677cd775145
Nguyen Manh Tuan <tuan.nguyenm5@nashtechglobal.com>
2018-06-12 18:17:07 +0700

    refactor

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,41 +4,45 @@ var gulp = require('gulp');
 // Include Our Plugins
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-// var livereload = require('gulp-livereload');
-// var minifyCss = require('gulp-cssnano');
+var plumber = require('gulp-plumber');
 let cleanCSS = require('gulp-clean-css');
 let sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var reload = browserSync.reload;
 
 // Compile Our Sass
 gulp.task('sass', function () {
-    return gulp.src('./styles/*.scss')
+    return gulp.src('./app/assets/styles/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(concat('styles.css'))
-        .pipe(cleanCSS({debug: true}, (details) => {
-            console.log(`${details.name}: ${details.stats.originalSize}`);
-            console.log(`${details.name}: ${details.stats.minifiedSize}`);
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('./styles'))
+        // .pipe(plumber({
+        //     errorHandler: function (error) {
+        //         console.log(error.toString());
+        //         this.emit('end');
+        //     }
+        // }))
+        .pipe(concat('styles.min.css'))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./app/assets/styles'))
         .pipe(browserSync.stream());
+});
+
+// Init server
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
+    });
 });
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+    browserSync.watch("./app/assets/styles/*.scss", ['sass']);
+    browserSync.watch("./app/assets/javascripts/**/*.js").on('change', reload);
+    browserSync.watch("./app/**/*.html").on('change', reload);
 
-    gulp.watch("./styles/scss/*.scss", ['sass']);
-    gulp.watch("./javascripts/*.js").on('change', reload);
-    gulp.watch("./*.html").on('change', reload);
 });
 
 // Default Task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'serve', 'watch']);
