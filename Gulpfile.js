ba724016a541fce45f3da1159bcdb23601a8f7f7
Nguyen Manh Tuan <tuan.nguyenm5@nashtechglobal.com>
2018-07-06 11:18:00 +0700

    fix css

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,26 +4,15 @@ var gulp = require('gulp');
 // Include Our Plugins
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-var plumber = require('gulp-plumber');
-let cleanCSS = require('gulp-clean-css');
-let sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 // Compile Our Sass
 gulp.task('sass', function () {
-    return gulp.src('./app/assets/styles/*.scss')
-        .pipe(sourcemaps.init())
-        // .pipe(plumber({
-        //     errorHandler: function (error) {
-        //         console.log(error.toString());
-        //         this.emit('end');
-        //     }
-        // }))
+    return gulp.src('./assets/styles/*.scss')
         .pipe(concat('styles.min.css'))
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./app/assets/styles'))
+        .pipe(sass())
+        .pipe(gulp.dest('./assets/styles'))
         .pipe(browserSync.stream());
 });
 
@@ -31,16 +20,16 @@ gulp.task('sass', function () {
 gulp.task('serve', function () {
     browserSync.init({
         server: {
-            baseDir: "./app"
+            baseDir: "./"
         }
     });
 });
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-    browserSync.watch("./app/assets/styles/*.scss", ['sass']);
-    browserSync.watch("./app/assets/javascripts/**/*.js").on('change', reload);
-    browserSync.watch("./app/**/*.html").on('change', reload);
+    browserSync.watch("./assets/styles/*.scss", ['sass']);
+    browserSync.watch("./assets/javascripts/**/*.js").on('change', reload);
+    browserSync.watch("./**/*.html").on('change', reload);
 
 });
 
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 #This is code for my web
 - My name: Tuan Nguyen Manh
 - Information web: About me
-- Web: http://nguyenmanhtuan93.github.io
-*****************************************
+- Web: https://nguyenmanhtuan93.github.io
